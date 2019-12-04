e2170e0a865bc63ff4ef29fc9629232b4ba4e1cc
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-12-04 16:27:49 -0600

    added helmet and logger middleware functionality

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+// installed helmet; currently not using
 
 const server = express();
+
+server.use(express.json()); // built in middleware
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +12,11 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url}`
+  );
+  next();
+}
 
 module.exports = server;
